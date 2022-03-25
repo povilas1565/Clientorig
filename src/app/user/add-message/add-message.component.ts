c66e0f3454bc0d4a593885b0a42125e2f40d42d8
paul156551 <pavel.ryzhkov.99@mail.ru>
2022-03-26 01:15:20 +0300

    Initial Commit

--- src/app/user/add-message/add-message.component.ts
+++ src/app/user/add-message/add-message.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Post} from "../../models/Post";
+import {Message} from "../../models/Message";
+import {NotificationService} from "../../services/notification.service";
+import {Router} from "@angular/router";
+import {MessageService} from "../../services/message.service";
 
 @Component({
   selector: 'app-add-message',
@@ -6,10 +12,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-message.component.css']
 })
 export class AddMessageComponent implements OnInit {
+  public messageEditForm: FormGroup;
+  public isMessageCreated = false;
+  public createdMessage: Message;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder,
+              private notificationService: NotificationService,
+              private messageService: MessageService,
+              private router: Router) {
   }
 
+  initMessageForm(): FormGroup {
+    return this.formBuilder.group({
+      title: ['', Validators.compose([Validators.required])],
+      location: ['', Validators.compose([Validators.required])],
+    });
+  }
+
+  ngOnInit(): void {
+    this.messageEditForm = this.initMessageForm();
+  }
+
+  submitMessageDate(): void {
+    this.messageService.createMessage({
+      title: this.messageEditForm.value.title,
+      location: this.messageEditForm.value.location
+    })
+      .subscribe(messageData => {
+        this.createdMessage = messageData;
+        console.log(messageData);
+      });
+  }
 }
--- src/app/user/add-post/add-post.component.ts
+++ src/app/user/add-post/add-post.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Post} from "../../models/Post";
+import {NotificationService} from "../../services/notification.service";
+import {PostService} from "../../services/post.service";
+import {ImageService} from "../../services/image.service";
+import {VideoService} from "../../services/video.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-add-post',
@@ -7,9 +14,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddPostComponent implements OnInit {
 
-  constructor() { }
+  public postEditForm: FormGroup;
+  public selectedFile: File;
+  public isPostCreated = false;
+  public createdPost: Post;
+  public previewImageUrl: any;
+  public previewVideoUrl: any;
 
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder,
+              private notificationService: NotificationService,
+              private postService: PostService,
+              private imageService: ImageService,
+              private videoService: VideoService,
+              private router: Router) {
   }
 
+  initPostForm(): FormGroup {
+    return this.formBuilder.group({
+      title: ['', Validators.compose([Validators.required])],
+      caption: ['', Validators.compose([Validators.required])],
+      location: ['', Validators.compose([Validators.required])],
+    });
+  }
+
+  ngOnInit(): void {
+    this.postEditForm = this.initPostForm();
+  }
+
+  submitPostDate(): void {
+    this.postService.createPost({
+      title: this.postEditForm.value.title,
+      caption: this.postEditForm.value.caption,
+      location: this.postEditForm.value.location})
+      .subscribe(postData => {
+        this.createdPost = postData;
+        console.log(postData);
+
+        if (this.createdPost.id ! == null) {
+          this.imageService.uploadImgToPost(this.selectedFile, this.createdPost.id)
+            .subscribe(() => {
+              this.notificationService.showSnackBar('Post was created');
+              this.isPostCreated = true;
+              this.router.navigate(['/profile']);
+            });
+        }
+
+        if (this.createdPost.id ! == null) {
+          this.videoService.uploadVidToPost(this.selectedFile, this.createdPost.id)
+            .subscribe(() => {
+              this.notificationService.showSnackBar('Post was created');
+              this.isPostCreated = true;
+              this.router.navigate(['/profile']);
+            });
+        }
+      });
+  }
+
+  onFileSelected(event: any): void {
+    this.selectedFile = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(this.selectedFile);
+    reader.onload = () => {
+      this.previewImageUrl = reader.result;
+      this.previewVideoUrl = reader.result;
+    };
+  }
 }
